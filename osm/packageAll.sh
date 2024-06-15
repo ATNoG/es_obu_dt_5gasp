69da88fb25daef3ec64349dabbfacf5935a0bf1e
Rafael Direito <rdireito@av.it.pt>
2024-06-15 14:44:56 +0000

    Added OSM's KNF and NS

---
+++ osm/packageAll.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# @Author: Rafael Direito
+# @Date:   2024-03-25 14:11:42
+# @Last Modified by:   Rafael Direito
+# @Last Modified time: 2024-03-25 14:11:54
+COPYFILE_DISABLE=true tar czvf es-obu-dt-5gasp-knf.tar.gz es-obu-dt-5gasp-knf
+COPYFILE_DISABLE=true tar czvf es-obu-dt-5gasp-ns.tar.gz es-obu-dt-5gasp-ns
